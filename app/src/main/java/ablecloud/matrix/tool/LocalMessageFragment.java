09127d054c74f78771f6812e634d2adb16993f1e
liuxiaofeng <liuxiaofeng@ablecloud.cn>
2017-11-13 16:06:37 +0800

    Feature: send to local device, allow custom msgCode

--- app/src/main/java/ablecloud/matrix/tool/LocalMessageFragment.java
+++ app/src/main/java/ablecloud/matrix/tool/LocalMessageFragment.java
@@ -4,6 +4,7 @@ import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.ActionBar;
+import android.text.TextUtils;
 import android.text.method.ScrollingMovementMethod;
 import android.text.util.Linkify;
 import android.view.LayoutInflater;
@@ -42,6 +43,8 @@ public class LocalMessageFragment extends Fragment {
     private static final SimpleDateFormat FORMAT = new SimpleDateFormat("HH:mm:ss.SSS");
     private static final Pattern LINKIFY_HEX = Pattern.compile("(^|\\s+)([\\da-fA-F]{2})+($|\\s+)");
 
+    @BindView(R.id.msgCode)
+    EditText msgCode;
     @BindView(R.id.request)
     EditText request;
     @BindView(R.id.log)
@@ -82,9 +85,14 @@ public class LocalMessageFragment extends Fragment {
 
     @OnClick(R.id.send)
     public void onViewClicked() {
+        String msgCode = this.msgCode.getText().toString().trim();
         final String requestMessage = request.getText().toString();
         final long requestTime = System.currentTimeMillis();
-        DeviceMessage deviceMessage = new DeviceMessage(68, ByteString.decodeHex(requestMessage).toByteArray());
+        if (TextUtils.isEmpty(msgCode) || TextUtils.isEmpty(requestMessage)) {
+            Toast.makeText(getActivity(), "msgCode和payload均不能为空", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        DeviceMessage deviceMessage = new DeviceMessage(Integer.parseInt(msgCode), ByteString.decodeHex(requestMessage).toByteArray());
         Matrix.bindManager().sendDevice(subDomain, physicalDeviceId, BindManager.Mode.LOCAL_ONLY, deviceMessage, new MatrixCallback<DeviceMessage>() {
             @Override
             public void success(DeviceMessage deviceMessage) {
